37ba1f782a860b22f8df9a85ca4b350025787053
Uday Patil <udaypatil@proton.me>
2023-05-17 17:03:20 +0530

    fixed router link

--- Frontend/news-classifier/src/App.vue
+++ Frontend/news-classifier/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link>
-    <router-link to="/predction">About</router-link>
+    <router-link to="/prediction">About</router-link>
   </nav>
   <router-view />
 </template>

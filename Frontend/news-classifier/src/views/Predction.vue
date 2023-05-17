819aafcad1cc4c24ba6c8e2970faabf8a407383a
Uday Patil <udaypatil@proton.me>
2023-05-17 16:52:22 +0530

    Replaced http with https

--- Frontend/news-classifier/src/views/Predction.vue
+++ Frontend/news-classifier/src/views/Predction.vue
@@ -49,7 +49,7 @@ export default {
                 body: `{"url":"${this.articleUrl}"}`
             };
 
-            fetch('http://64.227.158.87/predict', options)
+            fetch('https://64.227.158.87/predict', options)
                 .then(response => response.json())
                 .then(response => {
                     this.predictedCategory = response['predicted_category']
